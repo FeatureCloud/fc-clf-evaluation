dde0b092bf3a4e7cfc715640df78010480e5a0f3
Julian Späth <julian.spaeth@wzw.tum.de>
2021-02-22 17:06:31 +0100

    Implement roc curve federated

--- app/algo.py
+++ app/algo.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
-from sklearn.metrics import roc_auc_score, roc_curve
+import numpy as np
 import pandas as pd
+from sklearn.metrics import roc_auc_score, roc_curve
 
 
-def roc_plot(y_test, y_proba):
+def check(y_test, y_proba):
     if isinstance(y_test, pd.DataFrame):
         y_test = y_test.to_numpy().ravel()
     if isinstance(y_proba, pd.DataFrame):
@@ -15,10 +16,85 @@ def roc_plot(y_test, y_proba):
     except IndexError:
         pass
 
-    auc = roc_auc_score(y_test, y_proba)
-    print('AUC: %.3f' % auc)
+    return y_test, y_proba
 
-    fpr, tpr, thresholds = roc_curve(y_test, y_proba)
+
+def confusion_matrix(y_test, y_proba, threshold=0.5, positive_label=1):
+    tp = fp = tn = fn = 0
+    bool_actuals = [act == positive_label for act in y_test]
+    for truth, score in zip(bool_actuals, y_proba):
+        if float(score) > float(threshold):  # predicted positive
+            if truth:  # actually positive
+                tp += 1
+            else:  # actually negative
+                fp += 1
+        else:  # predicted negative
+            if not truth:  # actually negative
+                tn += 1
+            else:  # actually positive
+                fn += 1
+
+    return {"TP": tp, "FP": fp, "TN": tn, "FN": fn}
+
+
+def false_positive_rate(conf_mtrx):
+    return conf_mtrx["FP"] / (conf_mtrx["FP"] + conf_mtrx["TN"]) if (conf_mtrx["FP"] + conf_mtrx["TN"]) != 0 else 0
+
+
+def true_positive_rate(conf_mtrx):
+    return conf_mtrx["TP"] / (conf_mtrx["TP"] + conf_mtrx["FN"]) if (conf_mtrx["TP"] + conf_mtrx["FN"]) != 0 else 0
+
+
+def aggregate_confusion_matrices(confusion_matrices):
+    aggregates = []
+    for i in range(len(confusion_matrices[0])):
+        tp = fp = tn = fn = 0
+        for m in confusion_matrices:
+            tp += m[i]["TP"]
+            fp += m[i]["FP"]
+            tn += m[i]["TN"]
+            fn += m[i]["FN"]
+        agg = {"TP": tp, "FP": fp, "TN": tn, "FN": fn}
+        aggregates.append(agg)
+
+    return aggregates
+
+
+def compute_min_max_score(scores):
+    return min(scores), max(scores)
+
+
+def agg_compute_thresholds(local_min_max_scores):
+    min_scores = [score[0] for score in local_min_max_scores]
+    max_scores = [score[1] for score in local_min_max_scores]
+    low = min(min_scores)
+    high = max(max_scores)
+    step = (abs(low) + abs(high)) / 1000
+    thresholds = np.arange(low - step, high + step, step)
+
+    return thresholds
+
+
+def compute_threshold_conf_matrices(actuals, scores, thresholds):
+    # calculate confusion matrices for all thresholds
+    confusion_matrices = []
+    for threshold in thresholds:
+        confusion_matrices.append(confusion_matrix(actuals, scores, threshold))
+        # apply functions to confusion matrices
+    return confusion_matrices
+
+
+def compute_roc_parameters(confusion_matrices, thresholds):
+    # apply functions to confusion matrices
+    results = {"FPR": list(map(false_positive_rate, confusion_matrices)),
+               "TPR": list(map(true_positive_rate, confusion_matrices)),
+               "THR": thresholds}
+
+    return results
+
+
+def roc_plot(fpr, tpr, thresholds):
+    auc = compute_roc_auc(fpr, tpr)
 
     df = pd.DataFrame(data=[fpr, tpr, thresholds]).transpose()
     df.columns = ["fpr", "tpr", "thresholds"]
@@ -34,3 +110,10 @@ def roc_plot(y_test, y_proba):
     plt.legend()
 
     return plt, df
+
+
+def compute_roc_auc(fpr, tpr):
+    auc = -1 * np.trapz(tpr, fpr)
+
+    return auc
+
--- app/logic.py
+++ app/logic.py
@@ -3,10 +3,12 @@ import shutil
 import threading
 import time
 
+import jsonpickle
 import pandas as pd
 import yaml
 
-from app.algo import roc_plot
+from app.algo import roc_plot, check, compute_min_max_score, compute_threshold_conf_matrices, compute_roc_parameters, \
+    agg_compute_thresholds, aggregate_confusion_matrices
 
 
 class AppLogic:
@@ -43,6 +45,9 @@ class AppLogic:
         self.output_format = None
         self.y_test = None
         self.y_proba = None
+        self.thresholds = None
+        self.confusion_matrix = None
+        self.roc_params = None
         self.plt = None
         self.df = None
 
@@ -83,24 +88,33 @@ class AppLogic:
         # === States ===
         state_initializing = 1
         state_read_input = 2
-        state_plot = 3
-        state_writing_results = 4
-        state_finishing = 5
+        state_preprocess = 3
+        state_aggregate_thresholds = 4
+        state_wait_for_thresholds = 5
+        state_compute_confusion_matrix = 6
+        state_aggregate_confusion_matrices = 7
+        state_wait_for_confusion_matrices = 8
+        state_compute_roc = 9
+        state_writing_results = 10
+        state_finishing = 11
 
         # Initial state
         state = state_initializing
         self.progress = 'initializing...'
 
         while True:
+
+            # Local computations
+
             if state == state_initializing:
                 print("[CLIENT] Initializing")
                 if self.id is not None:  # Test if setup has happened already
                     state = state_read_input
                     print("[CLIENT] Coordinator", self.coordinator)
+
             if state == state_read_input:
                 print('[CLIENT] Read input and config')
                 self.read_config()
-
                 if self.y_test_filename.endswith(".csv"):
                     self.y_test = pd.read_csv(self.INPUT_DIR + "/" + self.y_test_filename, sep=",")
                 elif self.y_test_filename.endswith(".tsv"):
@@ -114,16 +128,69 @@ class AppLogic:
                     self.y_proba = pd.read_csv(self.INPUT_DIR + "/" + self.y_proba_filename, sep="\t")
                 else:
                     self.y_proba = pickle.load(self.INPUT_DIR + "/" + self.y_proba_filename)
-                state = state_plot
-            if state == state_plot:
-                print('[CLIENT] Compute and create plot')
-                self.plt, self.df = roc_plot(self.y_test, self.y_proba)
+                state = state_preprocess
+
+            if state == state_preprocess:
+                self.y_test, self.y_proba = check(self.y_test, self.y_proba)
+                min_score, max_score = compute_min_max_score(self.y_proba)
+
+                data_to_send = jsonpickle.encode([min_score, max_score])
+
+                if self.coordinator:
+                    self.data_incoming.append(data_to_send)
+                    state = state_aggregate_thresholds
+                else:
+                    self.data_outgoing = data_to_send
+                    self.status_available = True
+                    state = state_wait_for_thresholds
+                    print(f'[CLIENT] Sending computation data to coordinator', flush=True)
+
+            if state == state_wait_for_thresholds:
+                print("[CLIENT] Wait for thresholds")
+                self.progress = 'wait for thresholds'
+                if len(self.data_incoming) > 0:
+                    print("[CLIENT] Received aggregated thresholds from coordinator.")
+                    self.thresholds = jsonpickle.decode(self.data_incoming[0])
+                    self.data_incoming = []
+
+                    state = state_compute_confusion_matrix
+
+            if state == state_compute_confusion_matrix:
+                confusion_matrix = compute_threshold_conf_matrices(self.y_test, self.y_proba, self.thresholds)
+
+                data_to_send = jsonpickle.encode(confusion_matrix)
+
+                if self.coordinator:
+                    self.data_incoming.append(data_to_send)
+                    state = state_aggregate_confusion_matrices
+                else:
+                    self.data_outgoing = data_to_send
+                    self.status_available = True
+                    state = state_wait_for_confusion_matrices
+                    print(f'[CLIENT] Sending computation data to coordinator', flush=True)
+
+            if state == state_wait_for_confusion_matrices:
+                print("[CLIENT] Wait for confusion matrix")
+                self.progress = 'wait for confusion matrix'
+                if len(self.data_incoming) > 0:
+                    print("[CLIENT] Received aggregated confusion matrix from coordinator.")
+                    self.confusion_matrix = jsonpickle.decode(self.data_incoming[0])
+                    self.data_incoming = []
+
+                    state = state_compute_roc
+
+            if state == state_compute_roc:
+                print('[CLIENT] Compute roc parameters')
+                self.roc_params = compute_roc_parameters(self.confusion_matrix, self.thresholds)
                 state = state_writing_results
+
             if state == state_writing_results:
                 print('[CLIENT] Save results')
-                self.plt.savefig(self.OUTPUT_DIR + "/roc. " + self.output_format, format=self.output_format)
-                self.df.to_csv(self.OUTPUT_DIR + "/roc.csv", index=False)
+                plt, df = roc_plot(self.roc_params["FPR"], self.roc_params["TPR"], self.roc_params["THR"])
+                plt.savefig(self.OUTPUT_DIR + "/roc. " + self.output_format, format=self.output_format)
+                df.to_csv(self.OUTPUT_DIR + "/roc.csv", index=False)
                 state = state_finishing
+
             if state == state_finishing:
                 print("[CLIENT] Finishing")
                 self.progress = 'finishing...'
@@ -131,6 +198,34 @@ class AppLogic:
                     time.sleep(3)
                 self.status_finished = True
                 break
+
+            # GLOBAL AGGREGATIONS
+
+            if state == state_aggregate_thresholds:
+                print("[CLIENT] Aggregate thresholds")
+                self.progress = 'computing...'
+                if len(self.data_incoming) == len(self.clients):
+                    data = [jsonpickle.decode(client_data) for client_data in self.data_incoming]
+                    self.data_incoming = []
+                    self.thresholds = agg_compute_thresholds(data)
+                    data_to_broadcast = jsonpickle.encode(self.thresholds)
+                    self.data_outgoing = data_to_broadcast
+                    self.status_available = True
+                    state = state_compute_confusion_matrix
+                    print(f'[CLIENT] Broadcasting aggregated thresholds to clients', flush=True)
+
+            if state == state_aggregate_confusion_matrices:
+                print("[CLIENT] Aggregate confusion matricecs")
+                self.progress = 'computing...'
+                if len(self.data_incoming) == len(self.clients):
+                    data = [jsonpickle.decode(client_data) for client_data in self.data_incoming]
+                    self.data_incoming = []
+                    self.confusion_matrix = aggregate_confusion_matrices(data)
+                    data_to_broadcast = jsonpickle.encode(self.confusion_matrix)
+                    self.data_outgoing = data_to_broadcast
+                    self.status_available = True
+                    state = state_compute_roc
+                    print(f'[CLIENT] Broadcasting aggregated thresholds to clients', flush=True)
 
             time.sleep(1)
 
